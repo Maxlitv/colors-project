1c80ecf8cf811415cfa6f46c1b5c9013a21457f1
Maksim Litvinenka <be0999@c02r10tgg8wl.dk10.danskenet.net>
2020-05-08 17:58:05 +0300

    colors_continue

--- app.js
+++ app.js
@@ -7,22 +7,39 @@ let initialColors;
 
 // function - color generator
 function generateHex() {
-  const hexColor = chroma.random();
-  return hexColor;
+    const hexColor = chroma.random();
+    return hexColor;
 }
 let randomHex = generateHex();
 console.log(randomHex);
 
-//
+// we generate random colors on divs and div text
 function randomColors() {
-  colorDivs.forEach((div, index) => {
-    const hexText = div.children[0];
-    const randomColor = generateHex();
+    colorDivs.forEach((div, index) => {
+        const hexText = div.children[0];
+        const randomColor = generateHex();
 
-    //add color to background pls
-    div.style.backgroundColor = randomColor;
-    hexText.innerText = randomColor;
-  });
+        //add color to background pls
+        div.style.backgroundColor = randomColor;
+        hexText.innerText = randomColor;
+        //check for contrast
+        checkTextContrast(randomColor, hexText);
+        //sliders to work
+        const color = chroma(randomColor);
+        const sliders = div.querySelectorAll('.sliders input')
+        console.log(sliders);
+
+    });
+}
+
+// We check contrsat to show black/white color depending on luminance
+function checkTextContrast(color, text) {
+    const luminance = chroma(color).luminance();
+    if (luminance > 0.5) {
+        text.style.color = "black";
+    } else {
+        text.style.color = 'white'
+    }
 }
 
 randomColors();
