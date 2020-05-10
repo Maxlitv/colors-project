89131f8069bfeec90c973b60123538a569bfb3f4
Максим Литвиненко <maksimlitvinenka@gmail.com>
2020-05-10 14:52:58 +0300

    'sliders_range_activation'

--- app.js
+++ app.js
@@ -5,15 +5,20 @@ const sliders = document.querySelectorAll("input[type='range'] ");
 const currentHexes = document.querySelectorAll(".color h2");
 let initialColors;
 
+//event listeners
+//prettier-ignore
+sliders.forEach(slider => {
+  slider.addEventListener("input", hslControls);
+});
+
 // function - color generator
 function generateHex() {
   const hexColor = chroma.random();
   return hexColor;
 }
 let randomHex = generateHex();
-console.log(randomHex);
 
-// we generate random colors on color containers and div text
+// we generate random colors on color-containers and div-text
 function randomColors() {
   colorDivs.forEach((div, index) => {
     const hexText = div.children[0];
@@ -61,6 +66,27 @@ function colorizeSliders(color, hue, brightness, saturation) {
   brightness.style.backgroundImage = `linear-gradient(to right,${scaleBright(0)},${scaleBright(0.5)},${scaleBright(1)})`;
   // prettier-ignore
   hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75),rgb(204,204,75),rgb(75,204,75),rgb(75,204,204),rgb(75,75,204),rgb(204,75,204),rgb(204,75,75))`
+}
+
+function hslControls(e) {
+  const index =
+    e.target.getAttribute("data-bright") ||
+    e.target.getAttribute("data-sat") ||
+    e.target.getAttribute("data-hue");
+  let sliders = e.target.parentElement.querySelectorAll('input[type="range"]');
+  const hue = sliders[0];
+  const brightness = sliders[1];
+  const saturation = sliders[2];
+
+  const bgColor = colorDivs[index].querySelector("h2").innerText;
+  console.log(bgColor);
+
+  let color = chroma(bgColor)
+    .set("hsl.s", saturation.value)
+    .set("hsl.l", brightness.value)
+    .set("hsl.h", hue.value);
+
+  colorDivs[index].style.backgroundColor = color;
 }
 
 randomColors();
